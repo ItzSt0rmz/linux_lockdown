532fa559fa0ce91fff3e139b039d2e9f6544b44f
Cole Hawkins <120764143+ItzSt0rmz@users.noreply.github.com>
2024-10-20 14:34:41 -0500

    fadfadfasfdasfdsaf

--- PanSecurity.sh
+++ PanSecurity.sh
@@ -58,48 +58,13 @@ cp /etc/passwd ~/Desktop/backups/
 echo "/etc/group and /etc/passwd files backed up."
 
 echo "Type all user account names, with a space in between."
-read -a arr <<< "$users"
-
-for user in "${users[@]}"; do
-    echo $user
-done
-
-usersAuto=eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1
-echo usersAuto
+read -a users
 
 usersLength=${#users[@]}
-
 
 for (( i=0;i<$usersLength;i++))
 do
 	clear
-	echo ${users[${i}]}
-	echo "Delete ${users[${i}]}? yes or no"
-	read yn1
-	if [ $yn1 == y ]
-	then
-		userdel -r ${users[${i}]}
-		echo "${users[${i}]} has been deleted."
-	else
-
-		# echo "Make ${users[${i}]} administrator? yes or no"
-		# read yn2
-		# if [ $yn2 == y ]
-		# then
-		# 	gpasswd -a ${users[${i}]} sudo
-		# 	gpasswd -a ${users[${i}]} adm
-		# 	gpasswd -a ${users[${i}]} lpadmin
-		# 	gpasswd -a ${users[${i}]} sambashare
-		# 	echo "${users[${i}]} has been made an administrator."
-		# else
-		# 	gpasswd -d ${users[${i}]} sudo
-		# 	gpasswd -d ${users[${i}]} adm
-		# 	gpasswd -d ${users[${i}]} lpadmin
-		# 	gpasswd -d ${users[${i}]} sambashare
-		# 	gpasswd -d ${users[${i}]} root
-		# 	echo "${users[${i}]} has been made a standard user."
-		# fi
-
 		if [ $pwyn == y ]
 		then
 			echo -e "$pw\n$pw" | passwd ${users[${i}]}
@@ -109,9 +74,7 @@ do
 			echo "${users[${i}]} has been given the password 'Kraken1!'."
 		fi
 		passwd -x30 -n3 -w7 ${users[${i}]}
-		#usermod -L ${users[${i}]} #is this needed? and does it unlock? what about the current user?
 		echo "${users[${i}]}'s password has been given a maximum age of 30 days, minimum of 3 days, and warning of 7 days." # ${users[${i}]}'s account has been locked."
-	fi
 done
 clear
 
